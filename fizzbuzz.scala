47cbf64a41cf6fa6445c58a7d84eb5a18d079955
Kota Mizushima <mizukota@gmail.com>
2016-11-29 17:03:28 +0900

    Indent should be 2 in Scala

--- fizzbuzz.scala
+++ fizzbuzz.scala
@@ -1,15 +1,15 @@
 object FizzBuzz{
-      def main(args: Array[String]){
-        for(i <- 1 to 100) {
-          if(i % 3 == 0 && i % 5 == 0) {
-            println(i + "FizzBuzz")
-          }else if (i % 3 == 0) {
-            println(i + "Fizz")
-          }else if (i % 5 == 0) {
-            println(i + "Buzz")
-          }else {
-            println(i)
-          }
-        }
+  def main(args: Array[String]){
+    for(i <- 1 to 100) {
+      if(i % 3 == 0 && i % 5 == 0) {
+        println(i + "FizzBuzz")
+      }else if (i % 3 == 0) {
+        println(i + "Fizz")
+      }else if (i % 5 == 0) {
+        println(i + "Buzz")
+      }else {
+        println(i)
+      }
     }
+  }
 }
