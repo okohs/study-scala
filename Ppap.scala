b494975e644c79ef1689bbb277f53c89aee1b80b
Syoko Matsumura <fly.child1017@gmail.com>
2016-12-04 07:59:47 +0900

    Delete Ppap.scala

--- Ppap.scala
+++
@@ -1,9 +0,0 @@
-object Main extends App{
-  import scala.util.Random
-  
-  def ppap(pp: List[String] = Nil): Unit = pp match {
-    case "ペン" :: "パイナップル" :: "アップル" :: "ペン" :: _ => println(pp.reverse.mkString + "Pen Pineapple Apple Pen")
-    case _ => ppap((if(Random.nextBoolean) "ペン" else if(Random.nextBoolean) "パイナップル" else "アップル") :: pp)
-  }
-  ppap()
-}
