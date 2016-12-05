e2b142d813343c71d60dd3c5f8c83ccacc59bb6d
MATSUMURA Syoko <matsumura-s@gnavi.co.jp>
2016-12-05 17:36:02 +0900

    update PR comment

--- Ppap.scala
+++ Ppap.scala
@@ -1,10 +1,9 @@
 object Main extends App{
   import scala.util.Random
-  
+
   def ppap(pp: List[String] = Nil): Unit = pp match {
-    case "pen" :: "pineapple" :: "apple" :: "pen" :: _ => println(pp.reverse.mkString + " Ah-! pen-pineapple-Apple-pen!")
-    case _ => ppap((if(Random.nextBoolean) "Pen" else if(Random.nextBoolean) "Pineapple" else "apple") :: pp)
+    case "pen" :: "pineapple" :: "apple" :: "pen" :: _ => println(pp.reverse.mkString(" ") + " Ah-! pen-pineapple-Apple-pen!")
+    case _ => ppap((if(Random.nextBoolean) "pen" else if(Random.nextBoolean) "pineapple" else "apple") :: pp)
   }
   ppap()
 }
-
