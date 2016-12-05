1168bb5e2029c58224dd04dea7ddcc73b78447ac
MATSUMURA Syoko <matsumura-s@gnavi.co.jp>
2016-12-05 17:40:38 +0900

    fix match pattern

--- Ppap.scala
+++ Ppap.scala
@@ -2,7 +2,7 @@ object Main extends App{
   import scala.util.Random
 
   def ppap(pp: List[String] = Nil): Unit = pp match {
-    case "pen" :: "pineapple" :: "apple" :: "pen" :: _ => println(pp.reverse.mkString(" ") + " Ah-! pen-pineapple-Apple-pen!")
+    case "apple" :: "pen" :: "pineapple" :: "pen" :: _ => println(pp.reverse.mkString(" ") + " Ah-! pen-pineapple-Apple-pen!")
     case _ => ppap((if(Random.nextBoolean) "pen" else if(Random.nextBoolean) "pineapple" else "apple") :: pp)
   }
   ppap()
