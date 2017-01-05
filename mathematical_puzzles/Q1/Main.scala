4b6f7da4a56ee46b46af696de719c7e10488cd42
MATSUMURA Syoko <matsumura-s@gnavi.co.jp>
2017-01-05 12:30:38 +0900

    format code

--- mathematical_puzzles/Q1/Main.scala
+++ mathematical_puzzles/Q1/Main.scala
@@ -1,13 +1,13 @@
-object Main extends App{
-  val answer = Iterator.from(10, 1).filter{i=>
+object Main extends App {
+  val answer = Iterator.from(10, 1).filter { i =>
     isPalindrome(Integer.toString(i)) &&
-    isPalindrome(Integer.toOctalString(i)) &&
-    isPalindrome(Integer.toBinaryString(i))
+      isPalindrome(Integer.toOctalString(i)) &&
+      isPalindrome(Integer.toBinaryString(i))
   }.take(1)
   println(answer)
 
-  def isPalindrome(base:String): Boolean = {
-    val reverse:String = base.reverse
+  def isPalindrome(base: String): Boolean = {
+    val reverse: String = base.reverse
     base == reverse
   }
 }
