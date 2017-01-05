3990923238ffcd36a90f1d760e96a10b8545c3a8
MATSUMURA Syoko <matsumura-s@gnavi.co.jp>
2017-01-05 15:20:17 +0900

    change from object to top-level

--- mathematical_puzzles/Q1/Main.scala
+++ mathematical_puzzles/Q1/Main.scala
@@ -1,13 +1,11 @@
-object Main extends App {
-  val answer = Iterator.from(10, 1).filter { i =>
-    isPalindrome(Integer.toString(i)) &&
-      isPalindrome(Integer.toOctalString(i)) &&
-      isPalindrome(Integer.toBinaryString(i))
-  }.next
-  println(answer)
+val answer = Iterator.from(10, 1).filter { i =>
+  isPalindrome(Integer.toString(i)) &&
+    isPalindrome(Integer.toOctalString(i)) &&
+    isPalindrome(Integer.toBinaryString(i))
+}.next
+println(answer)
 
-  def isPalindrome(base: String): Boolean = {
-    val reverse: String = base.reverse
-    base == reverse
-  }
+def isPalindrome(base: String): Boolean = {
+  val reverse: String = base.reverse
+  base == reverse
 }
