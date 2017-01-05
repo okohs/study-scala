279333e55073e12f3844971da515a2e0d1b20d2f
MATSUMURA Syoko <matsumura-s@gnavi.co.jp>
2017-01-05 12:12:31 +0900

    use Iterator instead of while and Breaks

--- mathematical_puzzles/Q1/Main.scala
+++ mathematical_puzzles/Q1/Main.scala
@@ -1,17 +1,10 @@
-import scala.util.control.Breaks
-
 object Main extends App{
-  var i:Int = 10
-
-  while(true){
-    if(isPalindrome(Integer.toString(i)) && isPalindrome(Integer.toOctalString(i)) && isPalindrome(Integer.toBinaryString(i))){
-      println(i)
-      val b = new Breaks
-      b.break()
-    }else{
-      i+=1
-    }
-  }
+  val answer = Iterator.from(10, 1).filter{i=>
+    isPalindrome(Integer.toString(i)) &&
+    isPalindrome(Integer.toOctalString(i)) &&
+    isPalindrome(Integer.toBinaryString(i))
+  }.take(1)
+  println(answer)
 
   def isPalindrome(base:String): Boolean = {
     val reverse:String = base.reverse
