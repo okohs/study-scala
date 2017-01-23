e9b84b80266c31ac41c0c755a65e4e25ffa25028
Syoko Matsumura <fly.child1017@gmail.com>
2017-01-23 18:07:01 +0900

    Update Main.scala

--- mathematical_puzzles/Q2/Main.scala
+++ mathematical_puzzles/Q2/Main.scala
@@ -1,16 +1,21 @@
-val START:Integer = 1000
-val END:Integer = 9999
+val START_NUMBER:Int = 1000
+val END_NUMBER:Int = 9999
 
-// FIXME:END指定するならどこでやろう
-val answer  = Iterator.from(START, 1).filter {
-  i=> isSameReverseNumber(Integer.toString(i))
-}.next()
+val answer = Iterator.from(START_NUMBER, 1).filter(
+  i => (i.toString.reverse == calcNumber(i)) && (i <= END_NUMBER)
+).next()
 println(answer)
 
-// 並んでいる数字の各桁の間に四則演算の演算子を入れて計算する（いれなくてもよいが、最低でも一つ入れる）
-def calcNumber(base:Integer): Integer = {
+/**
+  * 並んでいる数字の各桁の間に四則演算の演算子を入れて計算する
+  * いれなくてもよいが、最低でも一つ入れる
+  *
+  * @param base
+  * @return
+  */
+def calcNumber(base:Int): Int = {
   // 並んでいる数字の各桁の間に四則演算の演算子を入れる
-
+  // 少なくとも１つ四則演算の演算子が入っているかチェックする
   // 計算する
   base
 }
